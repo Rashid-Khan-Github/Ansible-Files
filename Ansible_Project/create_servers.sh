0f016b9913a8026d989284dfe48491692f5f41d2
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-26 20:01:51 +0530

    Files Modified

--- Ansible_Project/create_servers.sh
+++ Ansible_Project/create_servers.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-NAMES=("mongodb" "catalogue" "web")
+NAMES=("mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-0f3c7d07486cad139
 SECURITY_GROUP_ID=sg-03e2563eefcedfe47
@@ -12,9 +12,9 @@ for i in "${NAMES[@]}"
 do
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
-        INSTANCE_TYPE="t3.micro"
+        INSTANCE_TYPE="t2.micro"
     else
-        INSTANCE_TYPE="t3.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags= [{Key=Name, Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
